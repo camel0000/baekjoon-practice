1a64257e3ccc3eb006a4683628dd651616593813
Mingi Cho <82951045+camel0000@users.noreply.github.com>
2022-07-31 03:48:40 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 836 ms, Memory: 64880 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -0,0 +1,30 @@
+# DFS & BFS (깊이우선탐색 & 너비우선탐색)
+# 11724번 연결 요소의 개수
+
+import sys
+sys.setrecursionlimit(3000)
+
+def dfs(v):
+    visited[v] = 1
+    
+    for i in G[v]:
+        if not visited[i]:
+            dfs(i)
+
+N, M = map(int, input().split())
+
+G = [[] for _ in range(N + 1)]
+for _ in range(M):
+    x, y = map(int, sys.stdin.readline().split())
+    G[x].append(y)
+    G[y].append(x)
+
+visited = [0] * (N + 1)
+count_ = 0
+
+for i in range(1, N + 1):
+    if not visited[i]:
+        dfs(i)
+        count_ += 1
+
+print(count_)
